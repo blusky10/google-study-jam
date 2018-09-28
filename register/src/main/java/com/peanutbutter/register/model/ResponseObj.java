93fb115a469c5d551fe07c4d287bb43aa97cc3e8
blusky0910@gmail.com <blusky0910@gmail.com>
2018-09-28 14:32:53 +0900

    Update: Serializable 추가

--- register/src/main/java/com/peanutbutter/register/model/ResponseObj.java
+++ register/src/main/java/com/peanutbutter/register/model/ResponseObj.java
@@ -3,12 +3,13 @@ package com.peanutbutter.register.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.net.URI;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
-public class ResponseObj {
+public class ResponseObj implements Serializable {
 
     private URI uri;
     private Timestamp expires;
