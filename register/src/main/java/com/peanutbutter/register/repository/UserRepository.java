3fa6fc83fda714edf0f0a3f84a948f6b05ee8213
blusky0910@gmail.com <blusky0910@gmail.com>
2018-10-02 13:04:55 +0900

    Update: 라이브러리 추가 & user repo 수정

--- register/src/main/java/com/peanutbutter/register/repository/UserRepository.java
+++ register/src/main/java/com/peanutbutter/register/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.peanutbutter.register.repository;
 
-import com.peanutbutter.register.model.User;
+import com.peanutbutter.register.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
